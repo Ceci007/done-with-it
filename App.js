3fab9c0604dab1d875ae963ac6a56fbfd9f99f12
solbencas <cecibenitezca@gmail.com>
2020-12-03 12:45:31 -0300

    Add WelcomeScreen.js

--- App.js
+++ App.js
@@ -1,25 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { View } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   return (
-    <View
-      style = {{
-        backgroundColor: "#fff",
-        flex: 1,
-      }}
-    >
-      <View 
-        style = {{ backgroundColor: "dodgerblue", flex: 2,}}
-      />
-       <View 
-        style = {{ backgroundColor: "gold", flex: 1,}}
-      />
-       <View 
-        style = {{ backgroundColor: "tomato", flex: 1,}}
-      />
-    </View>
+    <WelcomeScreen />
   );
 }
 
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,14 +1,46 @@
 import React from 'react';
-import { ImageBackground } from 'react-native';
+import { ImageBackground, View, Image, Text, StyleSheet } from 'react-native';
 
 function WelcomeScreen(props) {
   return (
-    <ImageBackground source={require('../assets/background.jpg')}></ImageBackground>
+    <ImageBackground 
+      style = {styles.background}
+      source={require('../assets/background.jpg')} >
+        <View style = {styles.logoContainer}>
+          <Image style = {styles.logo} source={require('../assets/logo-red.png')} />
+          <Text>Sell what you don't need</Text>
+        </View>
+        <View style = {styles.loginButton}></View>
+        <View style = {styles.registerButton}></View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  background: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  }, 
+  loginButton: {
+    width: '100%',
+    height: 70,
+    backgroundColor: '#fc5c65',
+  },
+  logo: {
+    width: 100,
+    height: 100, 
+  },
+  logoContainer: {
+    position: 'absolute',
+    top: 70,
+    alignItems: 'center',
+  },
+  registerButton: {
+    width: '100%',
+    height: 70,
+    backgroundColor: '#4ecdc4',
+  },
 })
 
 export default WelcomeScreen;
