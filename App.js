eb7a485fba66264b9a47c0dd015194fa3980ea4a
solbencas <cecibenitezca@gmail.com>
2020-12-23 14:27:10 -0300

    Improving ListingEditScreen

--- App.js
+++ App.js
@@ -4,26 +4,11 @@ import * as Permissions from 'expo-permissions';
 
 import Screen from './app/components/Screen';
 import ImageInputList from './app/components/ImageInputList';
+import ListingEditScreen from './app/screens/ListingEditScreen';
 
 export default function App() {
-  const [imageUris, setImageUris] = useState([]); 
-
-  const handleAdd = uri => {
-    setImageUris([...imageUris, uri]);
-  }
-
-  const handleRemove = uri => {
-    setImageUris(imageUris.filter(imageUri => imageUri !== uri));
-  }
-
   return (
-    <Screen>
-      <ImageInputList 
-        imageUris={imageUris}
-        onAddImage={handleAdd}
-        onRemoveImage={handleRemove} 
-      />
-    </Screen>
+    <ListingEditScreen /> 
   );
 }
 
