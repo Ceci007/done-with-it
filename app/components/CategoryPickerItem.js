1f8e76f34e68967aa86fb1afdd35640a048fb5fa
solbencas <cecibenitezca@gmail.com>
2020-12-23 16:35:07 -0300

    Add useLocation hook

--- app/components/CategoryPickerItem.js
+++ app/components/CategoryPickerItem.js
@@ -1,27 +1,36 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import Icon from './Icon';
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import colors from "../config/colors";
+
+import Icon from "./Icon";
 
 function CategoryPickerItem({ item, onPress }) {
   return (
     <View style={styles.container}>
-      <Icon backgroundColor={item.backgroundColor} name={item.icon} size={80} />
+      <TouchableOpacity onPress={onPress}>
+        <Icon
+          backgroundColor={item.backgroundColor}
+          name={item.icon}
+          size={80}
+        />
+      </TouchableOpacity>
       <Text style={styles.label}>{item.label}</Text>
     </View>
   );
-} 
+}
 
 const styles = StyleSheet.create({
   container: {
     paddingHorizontal: 30,
     paddingVertical: 15,
-    alignItems: 'center',
-    width: '33%',
+    alignItems: "center",
+    width: "33%",
   },
   label: {
     marginTop: 5,
-    textAlign: 'center',
+    textAlign: "center",
+    color: colors.grey,
   },
-})
+});
 
 export default CategoryPickerItem;
--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -2,15 +2,14 @@ import React from "react";
 import { StyleSheet } from "react-native";
 import * as Yup from "yup";
 
-import {
-  AppForm as Form,
-  AppFormField as FormField,
-  AppFormPicker as Picker,
-  SubmitButton,
-} from "../components/forms";
+import AppForm from '../components/forms/Form';
+import AppFormField from '../components/forms/FormField';
+import AppFormPicker from '../components/forms/FormPicker';
+import SubmitButton from '../components/forms/SubmitButton';
 import Screen from "../components/Screen";
 import CategoryPickerItem from "../components/CategoryPickerItem";
 import FormImagePicker from "../components/forms/FormImagePicker";
+import useLocation from "../hooks/useLocation";
 
 const validationSchema = Yup.object().shape({
   title: Yup.string().required().min(1).label("Title"),
@@ -78,9 +77,11 @@ const categories = [
 ];
 
 function ListingEditScreen() {
+  const location = useLocation();
+
   return (
     <Screen style={styles.container}>
-      <Form
+      <AppForm
         initialValues={{
           title: "",
           price: "",
@@ -88,26 +89,26 @@ function ListingEditScreen() {
           category: null,
           images: []
         }}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={(values) => console.log(location)}
         validationSchema={validationSchema}
       >
         <FormImagePicker name='images' />
-        <FormField maxLength={255} name="title" placeholder="Title" />
-        <FormField
+        <AppFormField maxLength={255} name="title" placeholder="Title" />
+        <AppFormField
           keyboardType="numeric"
           maxLength={8}
           name="price"
           placeholder="Price"
           width={120}
         />
-        <Picker 
+        <AppFormPicker
           items={categories} 
           name="category"
           numberOfColumns={3} 
           PickerItemComponent={CategoryPickerItem}
           placeholder="Category" 
           width='60%' />
-        <FormField
+        <AppFormField
           maxLength={255}
           multiline
           name="description"
@@ -115,7 +116,7 @@ function ListingEditScreen() {
           placeholder="Description"
         />
         <SubmitButton title="Submit" />
-      </Form>
+      </AppForm>
     </Screen>
   );
 }
