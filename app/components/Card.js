d0af6f0f02e5a963426e7756e9f4a3f151501d54
solbencas <cecibenitezca@gmail.com>
2020-12-26 14:34:21 -0300

    Connect frontend to backend and fetch the listings, add loading animation

--- app/components/Card.js
+++ app/components/Card.js
@@ -3,11 +3,11 @@ import { View, Image, Text, TouchableWithoutFeedback, StyleSheet } from 'react-n
 
 import colors from '../config/colors';
 
-function Card({title, subTitle, image, onPress}) {
+function Card({title, subTitle, imageUrl, onPress}) {
   return (
     <TouchableWithoutFeedback onPress={onPress}>
       <View style={styles.card}>
-        <Image style={styles.image} source={image} />
+        <Image style={styles.image} source={{ uri: imageUrl }} />
         <View style={styles.detailsContainer}>
           <Text style={styles.title} numberOfLines={1}>{title}</Text>
           <Text style={styles.subTitle} numberOfLines={2}>{subTitle}</Text>
--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,49 +1,65 @@
-import React from 'react';
-import { FlatList, StyleSheet } from 'react-native';
+import React, { useEffect} from 'react';
+import { FlatList, View, Text, StyleSheet } from 'react-native';
 
 import Screen from '../components/Screen';
 import Card from '../components/Card';
 import colors from '../config/colors';
-
-const listings = [
-  {
-    id: 1,
-    title: 'Red jacket for sale',
-    price: 100,
-    image: require('../assets/jacket.jpg')
-  },
-  {
-    id: 2,
-    title: 'Couch in great condition',
-    price: 1000,
-    image: require('../assets/couch.jpg')
-  },
-];
+import listingsApi from "../api/listings";
+import routes from "../navigation/routes";
+import AppButton from '../components/Button';
+import ActivityIndicator from '../components/ActivityIndicator';
+import useApi from "../hooks/useApi";
 
 function ListingsScreen({ navigation }) {
+  const getListingsApi = useApi(listingsApi.getListings);
+
+  useEffect(() => {
+    getListingsApi.request();
+  }, []);
+
   return (
     <Screen style={styles.screen}>
+      { getListingsApi.error && 
+      <>
+        <View style={styles.container}>
+          <Text style={styles.text}>Couldn't retrieve the listings.</Text>
+          <AppButton title='Retry' onPress={getListingsApi.request} />
+        </View>
+      </>}
+      <ActivityIndicator visible={getListingsApi.loading} />
       <FlatList 
-        data={listings}
+        data={getListingsApi.data}
         keyExtractor={listing => listing.id.toString()}
         renderItem={({item}) => 
           <Card 
             title = {item.title}
             subTitle = {'$' + item.price}
-            image={item.image}
-            onPress={() => navigation.navigate('ListingDetails', item)}
+            imageUrl={item.images[0].url}
+            onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
           />
         }
-      />
+      /> 
     </Screen>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    padding: 10,
+    marginVertical: 10,
+    borderColor: colors.danger,
+    borderWidth: 1,
+    backgroundColor: colors.lightRed,
+  }, 
   screen: {
     padding: 20,
     backgroundColor: colors.lightGrey,
-  }
+  },
+  text: {
+    color: colors.danger,
+    textAlign: 'center',
+  },
 })
 
 export default ListingsScreen;
